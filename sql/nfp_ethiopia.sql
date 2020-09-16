4e9f6bbfc1c8a9d92c5fca39eaec532d0b63c9d5
Cody Lanier <codylanier09@gmail.com>
2020-09-16 13:33:21 -0700

    fixed vanilla and updated foriegn lang texts

--- sql/nfp_ethiopia.sql
+++ sql/nfp_ethiopia.sql
@@ -42,19 +42,3 @@ INSERT OR IGNORE INTO ModifierStrings (ModifierId, Context, Text) VALUES
     ('OROMO_HILLS_COMBAT_BBG', 'Preview', 'LOC_ABILITY_MENELIK_HILLS_COMBAT_STRENGTH_DESCRIPTION');
 INSERT OR IGNORE INTO UnitAbilityModifiers VALUES
     ('ABILITY_ETHIOPIAN_OROMO_CAVALRY', 'OROMO_HILLS_COMBAT_BBG');
-
-
---==================
--- Sweden
---==================
--- +50% prod to uiniversity replacement in secret societies
-INSERT OR IGNORE INTO TraitModifiers (TraitType , ModifierId)
-        VALUES
-        ('TRAIT_CIVILIZATION_NOBEL_PRIZE' , 'NOBEL_PRIZE_ALCHEMICAL_SOCIETY_BOOST' );
-INSERT OR IGNORE INTO Modifiers (ModifierId , ModifierType , SubjectRequirementSetId)
-        VALUES
-        ('NOBEL_PRIZE_ALCHEMICAL_SOCIETY_BOOST' , 'MODIFIER_PLAYER_CITIES_ADJUST_BUILDING_PRODUCTION' , null);
-INSERT OR IGNORE INTO ModifierArguments (ModifierId , Name , Value)
-        VALUES
-        ('NOBEL_PRIZE_ALCHEMICAL_SOCIETY_BOOST' , 'BuildingType' , 'BUILDING_ALCHEMICAL_SOCIETY'),
-        ('NOBEL_PRIZE_ALCHEMICAL_SOCIETY_BOOST' , 'Amount'       , '50');
